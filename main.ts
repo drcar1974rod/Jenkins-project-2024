2d6939243780bb368a2da46ae4768a0d16e85d45
root <root@ip-172-31-19-54.ec2.internal>
2024-11-01 01:31:47 +0000

    pushing the terrform command

---
+++ main.ts
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "aws_linux_2" {
+    count = 1
+    ami = "ami-0e54eba7c51c234f6"
+    instance_type = "t2.micro"
+    key_name = "ubunt_key"
+    tags = {
+        Name = "aws_linux_2"
+    }
+}
